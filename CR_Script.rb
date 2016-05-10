81c6e06e2bdeeebf2855e3b09cee6afad7873ae6
Dawei Huang <davidhuangdw@163.com>
2016-05-10 12:27:43 +0800

    CRR metric

--- CR_Script.rb
+++ CR_Script.rb
@@ -84,7 +84,15 @@ def collect_activities(activity_type)
 end
 
 def total_hours
-  (config[:activity_created_at_end].to_time - config[:activity_created_at_start].to_time)/1.hour
+  @total_hours ||=
+    (config[:activity_created_at_end].to_time + 1.day -
+      config[:activity_created_at_start].to_time)/1.hour
+end
+
+def headers
+  {
+    :Authorization => "Bearer "+config[:token]
+  }
 end
 
 
@@ -108,6 +116,17 @@ def group_by_user(activities)
   activities.group_by do |act|
     act['actor']['id']
   end
+end
+
+def group_by_user_hour(activities)
+  grp_by_user = group_by_user(activities)
+  grp_by_user.map do |user_id, acts|
+    grp_by_hour = acts.group_by do |act|
+      Time.at((act['createdAt'].to_time.to_i/1.hour)*1.hour)
+    end.map.sort.to_h
+
+    [user_id, grp_by_hour]
+  end.to_h
 end
 
 # To be modified
@@ -146,35 +165,30 @@ params = {
   activityCreatedAtEnd: config[:activity_created_at_end].to_date.to_s,
   totalResults: true
 }
-headers = {
-  Authorization: "Bearer "+config[:token]
-}
 url = File.join(config[:api_url], config[:messages_path])
 
-message_result = {}
-
-ACTIVITY_TYPES.each do |activity_type|
+message_result = ACTIVITY_TYPES.map do |activity_type|
   params = params.merge(activityType: activity_type)
-  result = fetch_all(url, params: params, headers: headers)
+  messages = fetch_all(url, params: params, headers: headers)
 
-  message_result[activity_type.to_sym] = result
   # output_yml = act_yml_path(MESSAGES_OUTPUT_PATH, activity_type)
-  # File.write(output_yml, result.to_yaml)
-end
+  # File.write(output_yml, messages.to_yaml)
+  [activity_type.to_sym, messages]
+end.to_h
 
 
 ##################################
 ### 2. Expand all activities   ###
 ##################################
 
-message_result.each do |key, value|
+message_result.each do |act_type, acts|
   #msgs_yml = act_yml_path(MESSAGES_OUTPUT_PATH, activity_type)
-  messages = value
+  messages = acts
 
   messages['items'].each(&method(:expand_full_activities))
 
   if config[:output_raw_data]
-    msg_acts_yml = act_yml_path(MESSAGE_ACTIVITIES_OUTPUT_PATH, key)
+    msg_acts_yml = act_yml_path(MESSAGE_ACTIVITIES_OUTPUT_PATH, act_type)
     File.write(msg_acts_yml, messages.to_yaml)
   end
 
@@ -184,9 +198,24 @@ end
 ### 3. calculate productivity  ###
 ##################################
 
+activities = message_result.values.flat_map(&method(:parse_activities))
+# activities = ACTIVITY_TYPES.flat_map(&method(:collect_activities))
 
+user_activity_count = group_by_user(activities).map do|user_id, acts|
+  [user_id, acts.size.to_f/total_hours]
+end.to_h
 
+productivity_metric = {
+  'total_hours' => total_hours,
+  'activity_count_per_hour' =>
+    user_activity_count.merge('all' => activities.count.to_f/total_hours)
+}.to_yaml
 
+# File.write(PRODUCTIVITY_OUTPUT_PATH, productivity_metric)
+puts '========================='
+puts 'Productivity:'
+puts productivity_metric
+puts '========================='
 
 ##################################
 ###    4. calculate CRT        ###
@@ -196,11 +225,38 @@ end
 
 activities = message_result.values.flat_map(&method(:parse_activities))
 
-user_response_time = group_by_user(activities).map do |key, value|
-  { key => get_response_time(value)}
-end
+user_response_time = group_by_user(activities).map do |type, acts|
+  [type, get_response_time(acts)]
+end.to_h
 
-p user_response_time
+puts '========================='
+puts 'CRT(customer respond time):'
+puts user_response_time.to_yaml
+puts '========================='
 
+##################################
+###    5. calculate CRR        ###
+##################################
 
+activities = message_result.values.flat_map(&method(:parse_activities))
+# activities = ACTIVITY_TYPES.flat_map(&method(:collect_activities))
+
+params = {
+  bundleId: config[:bundle_id],
+  postedAtStart: config[:posted_at_start].to_date.to_s,
+  postedAtEnd: config[:posted_at_end].to_date.to_s,
+}
+msg_cnt_url = File.join(config[:api_url], config[:messages_count_path])
+resp = make_request(msg_cnt_url, params: params, headers: headers)
+total_messages_count = JSON.parse(resp.body)['count']
+
+respond_activities = activities.select{|act| act['type'] == 'respond'}
+respond_ratios = group_by_user(respond_activities).map do |user_id, acts|
+  [user_id, acts.size.to_f/total_messages_count]
+end.to_h
+
+puts '========================='
+puts 'CRR(customer respond ratio):'
+puts respond_ratios.to_yaml
+puts '========================='
 
